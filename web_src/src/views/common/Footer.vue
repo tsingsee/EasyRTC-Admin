3c39da25a69acbf16876c9f2160d2589721b4d90
Faker <1126080924@qq.com>
2020-08-19 17:34:14 +0800

    Change the logo image style of the login page

--- web_src/src/views/common/Footer.vue
+++ web_src/src/views/common/Footer.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="container_footer">
-    <span style="color:#808080">
-      Copyright &copy; 2014-{{ thisYear() }}
-      <a
-        href="http://www.tsingsee.com/"
-        style="color:#2a88d7"
-        target="_target"
-      >
-        <span
-          style="width: 70px;height: 16px;position: relative;overflow: hidden;display: inline-block;"
-        >
-          <i
-            class="iconfont iconqingxiLOGO"
-            style="font-size: 70px;position: absolute;top: -15px;left: 0;color:#2a88d7"
-          ></i>
-        </span>
-      </a>
-      .com
-      All rights reserved
-    </span>
+           <span style="color:#333333">
+        Copyright &copy; 2014-{{ thisYear()}}
+        <a href="http://www.tsingsee.com/" style="color:#2a88d7" target="_target">
+          <span
+            style="width: 78px;height: 16px;position: relative;overflow: hidden;display: inline-block;margin-left: -2px;"
+          >
+            <i
+              class="iconfont iconqingxiLOGO"
+              style="font-size: 78px;position: absolute;top: -15px;left: 0;color:#2a88d7"
+            ></i>
+          </span>
+        </a>.com. All rights reserved
+      </span>
   </div>
 </template>
 <script>
